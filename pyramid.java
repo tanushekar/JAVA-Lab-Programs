57d2c63fec22004b8e7fc6e80a78bff72ea6795f
tanushekar <tanushekar.015@gmail.com>
2025-09-19 13:18:36 +0530

    Create pyramid.java

---
+++ pyramid.java
@@ -0,0 +1,17 @@
+public class pyramid
+{
+    public static void main(String args[])
+    {
+        int n=Integer.parseInt(args[0]);
+        System.out.println("The pattern is: ");
+        
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
